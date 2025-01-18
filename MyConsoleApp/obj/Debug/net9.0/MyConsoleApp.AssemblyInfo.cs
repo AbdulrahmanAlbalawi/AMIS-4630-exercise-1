bdf9bcd8e7dbc1ffb90ac2ae265fb7c3915eba49
Abdulrahman Albalawi <d777mapps@gmail.com>
2025-01-18 13:27:07 -0500

    hi1

--- MyConsoleApp/obj/Debug/net9.0/MyConsoleApp.AssemblyInfo.cs
+++ MyConsoleApp/obj/Debug/net9.0/MyConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e804bf245396608abd2f70dae5b68ea04d1bd984")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f13b56fdd946d0a5b40fa8a99ffe0773d018c301")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
